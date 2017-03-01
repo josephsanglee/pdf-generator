4834c73d332eb546d89c4fa3ca9c8c088810419d
Joe <josephsanglee@gmail.com>
2017-02-28 23:45:06 -0800

    Basic implementation of images download to PDF

--- script.js
+++ script.js
@@ -24,35 +24,62 @@ const fetchTiles = (tileInformation) => {
 /***************************EVENT HANDLERS***************************/
 
 const exportPDF = () => {
+  $('#icon-text').empty();
+  $('#icon-text').append('Generating...');
   droneDeployAPI
   .then(fetchPlan)
   .then(fetchTileDataFromPlan)
   .then(fetchTiles)
   .then(tiles => {
     const doc = new jsPDF();
-    // const myImage = new Image();
-    const myHeaders = new Headers();
-    const myInit = {
-      method: 'GET',
-      headers: myHeaders,
-      mode: 'no-cors',
-      cache: 'default',
-    };
+    
+    //
+    const tilesPromises = tiles.map((tile) => {
+      return fetch(`https://cors-anywhere.herokuapp.com/${tile}`);
+    });
 
-    fetch(tiles[0], myInit)
-    .then(response => console.log(response));
+    Promise.all(tilesPromises)
+    .then((responses) => {
+      return Promise.all(responses.map(response => response.blob()));
+    })
+    .then((blobs) => {
+      return Promise.all(blobs.map(blob => URL.createObjectURL(blob)));
+    })
+    .then((urls) => {
+      const urlPromises = urls.map((url) => {
+        return new Promise((resolve, reject) => {
+          const image = new Image();
+          image.src = url;
 
-    // myImage.src = tiles[0];
-    // myImage.crossOrigin = 'Anonymous';
+          image.onload = () => {
+            doc.addImage(image, 'PNG', 15, 40, 180, 160);
+            resolve();
+          };
+        });
+      });
 
-    // myImage.onload = function() {
-    //   doc.addImage(myImage, 'png', 5, 5, 40, 10);
+      Promise.all(urlPromises)
+      .then((success) => {
+        $('#icon-text').empty();
+        $('#icon-text').append('Generate');
+        doc.save('map.pdf');
+      });
+    });
 
-    //   doc.save('map.pdf');
-    // };
+    console.log('hi');
   });
+
+  console.log('hitting the button');
 };
 
+/*fetch('https://cors-anywhere.herokuapp.com/https://public-tiles.dronedeploy.com/1487970337_JOSEPHSANGOPENPIPELINE_ortho_afi/17/30310/51502.png?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9wdWJsaWMtdGlsZXMuZHJvbmVkZXBsb3kuY29tLzE0ODc5NzAzMzdfSk9TRVBIU0FOR09QRU5QSVBFTElORV9vcnRob19hZmkvKiIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTQ5MDkzMzg3Mn19fV19&Signature=VuBqMmkcDL1HJiLUf1lOPJsAT8KaaEmnHv05YRBgxGfFLEg7dd21FX90PsAhUV3HA1bEd9GZ0tSqcnEP~C0CM3no2fJ360SnGFLsL-y7Iw6hOBZJODtKCys21xSUL~2TsHk-xaHCZqNSOBp7yE7sbZjR5DIVclSMqwrI1D3AEOjWq1DOuma4vLgjS11Hi2RTUJ8N5NP6ZzF0ldbpeKTIUwiWFhfV5xVw4Kodos8pattZfUEfpkBy9UESBem8oDOrXS9~z5M2mgkhbWP9b9HGufXpf3zQVpV2~nCottHCj8AIZ29Cxzdc9pOlnQHZk8x1~LnQPN4N47TLbIt38pCj~Q__&Key-Pair-Id=APKAJXGC45PGQXCMCXSA')
+    .then(function(response) {
+      return response.blob();
+}).then(function(blob) {
+  var objectURL = URL.createObjectURL(blob);
+  myImage.src = objectURL;
+});
+*/
 $('document').ready(() => {
   $('.pdf-button').on('click', exportPDF);
 });
